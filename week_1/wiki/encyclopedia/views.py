ce645a4111c6e99526b87f56a2ff0074bcb7737e
David Romero <pollinxld@gmail.com>
2023-10-11 23:05:41 -0600

    set up views, urls, and title.html

--- week_1/wiki/encyclopedia/views.py
+++ week_1/wiki/encyclopedia/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
+
+
+def html_converter(filename):
+    markdowner = Markdown()
+    entry = util.get_entry(filename)
+    if entry is None:
+        return None
+    else:
+        return markdowner.convert(entry)
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def title(request, title):
+    content = html_converter(title)
+    if content is not None:
+        return render(request, "encyclopedia/title.html", {
+            "title": title,
+            "content": content
+        })
 
