a2aee2a2d724f0543f12eb999156f0654f703fa0
David Romero <pollinxld@gmail.com>
2023-10-31 18:44:15 -0600

    add models and set admin

--- week_2/commerce/auctions/admin.py
+++ week_2/commerce/auctions/admin.py
@@ -3,3 +3,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Listing)
--- week_2/commerce/auctions/models.py
+++ week_2/commerce/auctions/models.py
@@ -4,3 +4,36 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+    bid = models.FloatField()
+
+    def __str__(self):
+        return self.bid
+
+
+class Comment(models.Model):
+    pass
+
+
+class Category(models.Model):
+    type = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.type
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    actual_bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="actualBid")
+    image = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
