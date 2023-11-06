92c12560251093921846f099d02a60b13966c277
David Romero <pollinxld@gmail.com>
2023-11-06 17:26:13 -0600

    display view-email

--- week_3/mail/mail/static/mail/inbox.js
+++ week_3/mail/mail/static/mail/inbox.js
@@ -16,6 +16,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#view-email').style.displey = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -24,6 +25,10 @@ function compose_email() {
 }
 
 function view_email(id) {
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#view-email').style.displey = 'block';
+  
   console.log(id)
 }
 
@@ -32,6 +37,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#view-email').style.displey = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><hr>`;
