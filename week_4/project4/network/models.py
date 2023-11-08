cc7e0b8c2461e0d3cdcf662e5018bd07972e93e0
David Romero <pollinxld@gmail.com>
2023-11-08 15:42:59 -0600

    add post model

--- week_4/project4/network/models.py
+++ week_4/project4/network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    post = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} on {self.date}"
