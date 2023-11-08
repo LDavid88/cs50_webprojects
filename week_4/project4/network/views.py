3000ffc0d429b9480fe5dcf5e2278770e6c4fa7e
David Romero <pollinxld@gmail.com>
2023-11-08 15:16:53 -0600

    add new post feature

--- week_4/project4/network/views.py
+++ week_4/project4/network/views.py
@@ -61,3 +61,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+def new_post(request):
+    return render(request, "network/index.html")
